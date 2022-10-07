5b23b6bf42b5c759969e5e96937053b65d1192c6
anfepema700 <anfepema700@github.com>
2022-10-07 10:53:38 -0500

    add nodemon and create index route

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app= express();
+const morgan = require('morgan');
+
+//setting
+app.set('port',process.env.PORT || 3000);
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//routes
+app.get('/',(req,res)=>{
+        res.send('Hello world');
+});
+
+
+//starting the server
+
+app.listen(app.get('port'),()=>{
+    console.log('Server on port ${app.get('port')}');
+}
+
+);
